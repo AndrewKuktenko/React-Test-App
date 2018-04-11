8ad8af2001bc24fcb9bbe9fb3bb43cd08693a9d9
AndrewKuktenko <andriy.kuktenko@gmail.com>
2018-04-11 10:20:02 +0300

    button added

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function Article() {
-    const body = <section>body</section>
-    return (
-        <div >
-            <h2>title</h2>
-            {body}
-            <h3>creation date: {(new Date()).toDateString()}</h3>
-        </div>
-    )
+class Article extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isOpen: true
+        }
+    }
+
+    // state = {
+    //     isOpen:true
+    // }
+
+    render() {
+        const {article} = this.props
+        console.log('----', this.props)
+        const body = this.state.isOpen && <section>{article.text}</section>
+        return (
+            <div >
+                <h2>
+                    {article.title}
+                    <button onClick={this.handleClick}>
+                        {this.state.isOpen ? 'Close' : 'Open'}
+                    </button>
+                </h2>
+                {body}
+                <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
+            </div>
+        )
+    }
+
+    handleClick = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
 }
 
 export default Article
