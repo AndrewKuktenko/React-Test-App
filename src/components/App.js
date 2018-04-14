be75d0c203679db89171e669fd71e40ba84b5ac6
AndrewKuktenko <andriy.kuktenko@gmail.com>
2018-04-14 23:58:34 +0300

    button

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ArticleList from './ArticleList'
 import articles from '../fixtures'
 import 'bootstrap/dist/css/bootstrap.css'
 
-function App() {
-    return (
-        <div className='container'>
-            <div className="jumbotron">
-                <h1 className="display-3">App Name</h1>
-            </div>
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            reverted: false
+        }
+    }
 
-            <ArticleList articles = {articles}/>
-        </div>
-    )
+
+    render() {
+
+        return(
+            <div className='container'>
+                <div className="jumbotron">
+                    <h1 className="display-3">App Name</h1>
+                    <button className="btn btn-info" onCkick={this.handleClick()}>App Name</button>
+                </div>
+
+                <ArticleList articles = {articles}/>
+            </div>
+        )
+    }
+
+    handleClick = () => {
+        this.setState({
+            reverted: !this.state.reverted
+        })
+    }
 }
 
 export default App
